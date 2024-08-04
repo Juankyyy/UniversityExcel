54a766cb7cf6c0039d4e9413128882be8b6f721e
Juankyyy <103221572+Juankyyy@users.noreply.github.com>
2024-08-04 18:52:08 -0500

    feat: Export Students Excel

--- Controllers/ExcelController.cs
+++ Controllers/ExcelController.cs
@@ -14,9 +14,46 @@ namespace University.Controllers
             _context = context;
         }
 
-        public FileResult ExportExcel (string FileName, IEnumerable<Student> Students)
+        public FileResult ExportExcel ()
         {
-            return null;
+            var students = _context.Students.ToList();
+
+            var fileName = "Students.xlsx";
+
+            using (var workbook = new XLWorkbook())
+            {
+                var worksheet = workbook.Worksheets.Add("Students");
+
+                worksheet.Cell(1, 1).Value = "Id";
+                worksheet.Cell(1, 2).Value = "Name";
+                worksheet.Cell(1, 3).Value = "Last Name";
+                worksheet.Cell(1, 4).Value = "Email";
+                worksheet.Cell(1, 5).Value = "Phone";
+
+                for (int i = 0; i < students.Count; i++)
+                {
+                    worksheet.Cell(i + 2, 1).Value = students[i].Id;
+                    worksheet.Cell(i + 2, 2).Value = students[i].Name;
+                    worksheet.Cell(i + 2, 3).Value = students[i].LastName;
+                    worksheet.Cell(i + 2, 4).Value = students[i].Email;
+                    worksheet.Cell(i + 2, 5).Value = students[i].Phone;
+                }
+
+                // Table Border
+                var range = worksheet.Range(1, 1, students.Count + 1, 5);
+
+                range.Style.Border.OutsideBorder = XLBorderStyleValues.Thin;
+                range.Style.Border.InsideBorder = XLBorderStyleValues.Thin;
+                range.Style.Border.OutsideBorderColor = XLColor.Black;
+                range.Style.Border.InsideBorderColor = XLColor.Black;
+
+                using (var stream = new MemoryStream())
+                {
+                    workbook.SaveAs(stream);
+                    var content = stream.ToArray();
+                    return File(content, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", $"{fileName}");
+                }
+            }
         }
 
         [HttpPost]
